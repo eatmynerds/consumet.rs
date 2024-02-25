756622799983d81af5493135fd466fe25c199145
eatmynerds <vipershniper09@gmail.com>
2024-02-25 07:23:39 +0000

    Update src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
+pub mod extractors;
+pub mod html;
+pub mod models;
+pub mod providers;
+pub mod utils;
+
 use lazy_static::lazy_static;
 use reqwest::Client;
 
 lazy_static! {
     static ref CLIENT: Client = Client::new();
 }
-
-pub mod extractors;
-pub mod html;
-pub mod models;
-pub mod providers;
-pub mod utils;
